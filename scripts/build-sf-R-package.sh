57ae3e209d981292304c9d7bdeabedf3c009d761
hokafu <107949897+hokafu@users.noreply.github.com>
2023-10-19 12:00:26 -0400

    Update build-sf-R-package.sh : comments version bump

--- scripts/build-sf-R-package.sh
+++ scripts/build-sf-R-package.sh
@@ -1,4 +1,4 @@
-#### build-sf-R-package.sh v1.3
+#### build-sf-R-package.sh v1.4
 # Build and install the "Simple Features for R" package
 # (https://github.com/r-spatial/sf) and dependencies from source
 #
@@ -111,6 +111,7 @@ curl -O https://download.osgeo.org/proj/proj-9.3.0.tar.gz
 #proj data if needed
 # curl -O https://download.osgeo.org/proj/proj-data-1.15.tar.gz
 curl -O https://sqlite.org/2023/sqlite-autoconf-3430100.tar.gz
+# if curl fails above
 #wget --no-proxy https://sqlite.org/2023/sqlite-autoconf-3430100.tar.gz
 wget https://cran.r-project.org/src/contrib/sf_1.0-14.tar.gz
 
